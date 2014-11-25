3307f6978e27203fceb5d365ac27a8bfa8107c60
Indi Knighton <44137@longroad.ac.uk>
2014-11-25 14:47:57 +0000

    This is the actual finished one

---
+++ test example.py
@@ -0,0 +1,39 @@
+#Indi Knighton
+#18/11/2014
+#Building functions
+
+def calculate_basic_pay(hours, pay):
+    total_pay = hours * pay
+    return total_pay
+
+def calculate_overtime_pay(hours, pay):
+    overtime_pay = (hours-40) * (pay*1.5)
+    basic_pay = 40 * pay
+    total_pay = basic_pay + overtime_pay
+    return total_pay
+
+def calculate_total_pay(hours, pay):
+    if hours <=40:
+        total_pay = calculate_basic_pay(hours, pay)
+    else:
+        total_pay =  calculate_overtime_pay(hours, pay)
+    return total_pay
+
+def work_details():
+    hours = int(input("Please enter how much hours you work weekly: "))
+    pay = float(input("Please enter how much you get payed hourly: "))
+    return hours, pay
+
+
+def display_total_pay(total_pay):
+    print(total_pay)
+    return total_pay
+
+def calculate_pay():
+    hours, pay = work_details()
+    total_pay = calculate_total_pay(hours, pay)
+    display_pay = display_total_pay(total_pay)
+    return total_pay
+
+#main program
+calculate_pay()
